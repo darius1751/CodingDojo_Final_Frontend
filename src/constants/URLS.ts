ac0fd329a5354011c070f416dfbde0fe01237900
darius1751 <lupequi12@gmail.com>
2024-11-05 23:45:54 -0500

    Apuntando a la api/v1

--- src/constants/URLS.ts
+++ src/constants/URLS.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.DEV ? 'http://localhost:4000/api/v1' : "https://codingdojo-final-backend.onrender.com";
+export const BASE_URL = import.meta.env.DEV ? 'http://localhost:4000/api/v1' : "https://codingdojo-final-backend.onrender.com/api/v1";
 console.log({ BASE_URL })
 export const URLS = Object.freeze({
     login: `${BASE_URL}/auth/login`,
